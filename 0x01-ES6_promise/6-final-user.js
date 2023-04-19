593087f80899d7ddc77c9807a19e1d2e22ccad37
palvin254 <palvinpaul900m@gmail.com>
2023-04-19 11:32:37 -0700

    0x01-ES6_promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,19 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import { signUpUser } from './4-user-promise.js';
+import { uploadPhoto } from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+export function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+    uploadPhoto(fileName)
+  ];
+
+  return Promise.allSettled(promises)
+    .then(results => {
+      return results.map(result => {
+        return {
+          status: result.status,
+          value: result.status === 'fulfilled' ? result.value : result.reason
+        };
+      });
+    });
 }
